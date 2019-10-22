bb9fe554e410033b9aa5da4ee9d1390a6b936381
sania <saniainf@gmail.com>
2019-10-22 17:50:21 +0700

    scriptable objects

---
+++ Assets/Scripts/Objects/PlayerObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Client
+{
+    [CreateAssetMenu(menuName = "EcsRoguelike/PlayerObject", fileName = "PlayerPreset")]
+    public class PlayerObject : ScriptableObject
+    {
+
+    }
+}
