6b559ddfff7eba54c955564d3bcd895fb4f8ec33
sania <saniainf@gmail.com>
2020-03-05 22:41:20 +0700

    34234234

---
+++ Assets/Scripts/Presets/Entities/CollectingItemPreset.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Client
+{
+    [CreateAssetMenu(menuName = "EcsRoguelike/Presets/Entities/CollectingItem", fileName = "CollectingItemPreset")]
+    class CollectingItemPreset : ScriptableObject
+    {
+        public GameObjectPreset GameObject;
+        public BuffPreset Buff;
+    }
+}
