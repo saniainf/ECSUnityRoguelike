c376838ed248111aebbe335afd785f8b4a571419
sania <saniainf@gmail.com>
2020-03-05 20:16:10 +0700

    123123

---
+++ Assets/Scripts/Components/ExitPointComponent.cs
@@ -0,0 +1,12 @@
+using Leopotam.Ecs;
+
+namespace Client
+{
+    sealed class ExitPointComponent : IEcsAutoReset
+    {
+        void IEcsAutoReset.Reset()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Presets/Entities/PlayerPreset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Client
+{
+    [CreateAssetMenu(menuName = "EcsRoguelike/Presets/Entities/Player", fileName = "PlayerPreset")]
+    class PlayerPreset : ScriptableObject
+    {
+        public GameObjectPreset GameObject;
+
+        [Header("Preferences")]
+        public int HealthPoint;
+        public int Initiative;
+        public WeaponItemObject PrimaryWeaponItem;
+        public WeaponItemObject SecondaryWeaponItem;
+    }
+}
